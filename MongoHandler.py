666528ecd555cd4cc2fa385051161f888ed2591b
David Tayar <xdavidt@gmail.com>
2019-08-05 20:12:16 +0300

    Revert "SYNC"
    This reverts commit 88a73e7e

--- MongoHandler.py
+++ MongoHandler.py
@@ -1,30 +1,34 @@
 from EvtManagerClass import *
+from datetime import datetime
 
 def db_connection():
     try:
         # TODO: fix pushing to DB
         meg.register_connection(alias='default',name ='test',host = '192.168.0.128',port = 27017)
-        print("Mongo try to connect")
+        print("Mongo try to connect")# Debug only
 
     except:
-        print("Fail to connect DB")
+        print("Fail to connect DB") # Debug only
 
 def pushToMongo(evtmgr) -> LogTemplate:
-    loghand = LogTemplate()
-    loghand.id = evtmgr.id
-    loghand.time = evtmgr.time
+    loghand = LogTemplate() # LogHandler in the Class
+    loghand.logid = evtmgr.id
+    loghand.time = datetime.fromtimestamp(evtmgr.time.seconds)
     loghand.type = evtmgr.type
     loghand.src = evtmgr.src
+    loghand.cat = evtmgr.cat
     loghand.hostname = evtmgr.hostname
     loghand.username = evtmgr.username
     loghand.os = evtmgr.os
-    if (evtmgr.dataList != None):
-        loghand.dataList = evtmgr.dataList
+
+
     try:
+        if (evtmgr.dataList != None):
+            loghand.dataList = list(evtmgr.dataList)
         loghand.save()
-        print("OK")  # For debug only
         return True
-    except:
+    except Exception as error_massage:
+        print('Push_to_mongo failure:\n %s' % error_massage) # Debug only
         return False
 
 
