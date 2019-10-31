dd2c2260d12d4659b1f6dfae79f54f4732eaaf5f
David Tayar <xdavidt@gmail.com>
2019-10-31 15:39:28 +0200

    Category from DB to logger

--- MongoC/ClientReport.py
+++ MongoC/ClientReport.py
@@ -1,10 +1,13 @@
 import mongoengine as meg
 
+meg.connect(host='mongodb+srv://siem:iCDoqbyTT3xh@cluster0-ecrrx.gcp.mongodb.net/clientManager?retryWrites=true&w=majority',
+            alias='clientManager',db='clientManager')
+
 class ClientReport(meg.Document):
     header = meg.StringField(required=True)
     desc = meg.StringField(required=True)
     time = meg.DateTimeField(required=True)
     meta = {
-        'alias': 'clientManager',
+        'db_alias': 'clientManager',
         'collection': 'ClientReports'
     }
